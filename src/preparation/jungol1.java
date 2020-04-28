0addd3638e27965a48135f0970b50a6a3bc6b8a1
yuri <dbfl_0414@naver.com>
2020-04-28 16:25:38 +0900

    준골연습예제1

---
+++ src/preparation/jungol1.java
@@ -0,0 +1,49 @@
+package preparation;
+
+import java.util.Scanner;
+
+public class jungol1 {
+
+	public static void main(String[] args) {
+		//출력 - 형성평가 1
+		//System.out.println("My name is Hong");
+		
+		//출력 - 형성평가 5
+		/*
+		String a1 = "Seoul";
+		String a2 = "Pusan";
+		String a3 = "Incheon";
+		String a4 = "Daegu";
+		String a5 = "Gwangju";
+		String b1 = "10,312,545";
+		String b2 = "3,567,910";
+		String b3 = "2,758,296";
+		String b4 = "2,511,676";
+		String b5 = "1,454,636";
+		String c1 = "+91,375";
+		String c2 = "+5,868";
+		String c3 = "+64,888";
+		String c4 = "+17,230";
+		String c5 = "+29,774";
+		
+		System.out.printf("%15s%15s%15s\n",a1,b1,c1);
+		System.out.printf("%15s%15s%15s\n",a2,b2,c2);
+		System.out.printf("%15s%15s%15s\n",a3,b3,c3);
+		System.out.printf("%15s%15s%15s\n",a4,b4,c4);
+		System.out.printf("%15s%15s%15s\n",a5,b5,c5);
+		*/
+		//입력 - 형성평가 1
+		/*
+		int a = 10,b = 20, c = 30;
+		System.out.printf("%d + %d = %d", a, b, c);
+		*/
+		//입력 - 형성평가 5
+
+		Scanner scan = new Scanner(System.in);
+		System.out.print("yard? ");
+		double yard = scan.nextDouble();
+		
+		
+	}
+
+}
