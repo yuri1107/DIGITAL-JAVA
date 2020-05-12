66fd76f46ce105ac74f8151ef4437735a37c8275
yuri <dbfl_0414@naver.com>
2020-05-12 09:23:34 +0900

    실습예제-주민등록번호로 남녀 구분하기
    선생님의 도움을받아 코드를 예쁘게함

--- src/assignment/MaleOrFemale.java
+++ src/assignment/MaleOrFemale.java
@@ -19,16 +19,28 @@ public class MaleOrFemale {
 		for ( ; ; ) {
 			System.out.println("주민번호를 입력하세요 :  종료를 원할경우 '종료' 입력 : ");
 			security = scan.nextLine();
-			if (security.contains("-")) {
-				
-				security.replaceAll("-", "");
-				female = security.charAt(7);
-				if (female % 2 == 0) {
-					System.out.println("해당 주민번호는 여성입니다.");
-				}else {System.out.println("해당 주민번호는 남성입니다.");}
-			}
 			
+			if (security.contains("종료")) {
+				System.out.println("종료합니다.");
+				break;
+			}
+
+			security = security.replaceAll("-", ""); // replace는 if문으로 조건 검사를 할 필요가 없다. 바꿀 애가없으면 걍 실행을 안함
+			security = security.replaceAll(" ", "");
+			System.out.println(security); // 문자열을 잘 정돈 했는지 확인하는 출력
+
+			if (security.length() == 13) {
+				female = security.charAt(6);
+				if(female % 2 == 0) {
+					System.out.println("해당 주민번호는 여성입니다.");
+				}else {
+					System.out.println("해당 주민번호는 남성입니다.");
+				}
+			}else if (!security.contains("종료")) {
+				System.out.println("잘못된 입력입니다.");
+			}
 		}
+		scan.close();
 
 	}
 
