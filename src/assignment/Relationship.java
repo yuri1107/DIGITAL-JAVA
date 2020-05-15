7ba0d64873afcb8399e899a8883eaa0808d6c7b7
yuri <dbfl_0414@naver.com>
2020-05-15 16:55:30 +0900

    17일차 과제 - 최소공배수 최대공약수

---
+++ src/assignment/Relationship.java
@@ -0,0 +1,23 @@
+package assignment;
+
+import java.util.Scanner;
+
+public class Relationship {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		
+		//예외처리 해야함
+	}
+
+	public static void greatest (int a, int b) { //최대공약수 구하는 메소드
+		
+	}
+	
+	public static void least () { //최소공배수 구하는 메소드
+		
+	}
+	
+}
